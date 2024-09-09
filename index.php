0ede64193d047dcb06cfa184e7c38578decce63d
dancalvin <danielcalvintaylorlds@gmail.com>
2024-09-08 19:39:56 -0600

    update

--- index.php
+++ index.php
@@ -36,8 +36,40 @@ $eviction_date = $_GET['eviction_date'] ?? '';
 </head>
 <body>
     <div class="container">
-        <h1>Promissory Note and Eviction Notice Preview</h1>
+        <h1>Eviction Notice and Promissory Note </h1>
         <div class="preview">
+
+
+<h2>Notice of Eviction - Notice to Pay or Quit</h2>
+
+This Notice is Given to Tenant:
+Name: <?php echo $name . "<br>"; ?>
+Address: <?php echo $address . "<br>"; ?>
+Phone: <?php echo $phone . "<br>"; ?>
+
+This Notice is Given by Landlord:
+Name: <?php echo $issuer_name . "<br>"; ?>
+Address: <?php echo $issuer_address . "<br>"; ?>
+Phone: <?php echo $issuer_phone . "<br>"; ?>
+
+You are hereby given notice that you are behind on your rent or other amounts due. You are required to either pay everything owed or vacate the premises.
+
+Within three (3) business days (or the appropriate cure period as stated in the lease) you are required to:
+
+1. Pay the following:
+   a. Rent due: $<?php echo $amount_due . "<br>"; ?>
+   b. Late fees: $<?php echo $late_fees . "<br>"; ?>
+   c. Miscellaneous fees: $<?php echo $misc_fees . "<br>"; ?>
+   Total: $<?php echo $total . "<br>"; ?>
+
+OR
+
+2. All occupants must vacate the premises.
+
+If you do not comply with this notice, you will be served with a Summons and Complaint for unlawful detainer. Unlawful detainer is when you remain in possession of rental property after the owner serves you with a lawful notice to leave, such as this eviction notice. If you are found by the court to be in unlawful detainer, you will be evicted and found liable for all amounts owed under the lease and/or Utah law, plus attorney fees, court costs, and three times those damages allowed to be trebled under Utah Code Ann. §78B-6-811.
+
+If your lease requires mediation, you must alert us in writing within three days of your willingness to participate in mediation. Mediation shall take place within seven days of receipt of your written notification. If you fail to provide this written notification within three days and/or you fail to participate in mediation within seven days, be advised that your landlord intends to proceed with legal or equitable relief.
+
 <h2>Promissory Note</h2>
 This Promissory Note is issued on <strong><?php echo $notice_date; ?>.</strong>
 
@@ -78,36 +110,6 @@ They agree to pay the total amount of <strong>$<?php echo $total; ?>. </strong>
 
 Signature: <input type="text" id="signature" class="signature-input" placeholder="Type your full name to sign">
 Date: <input type="date" id="signatureDate" class="signature-input">
-
-<h2>Notice of Eviction - Notice to Pay or Quit</h2>
-
-This Notice is Given to Tenant:
-Name: <?php echo $name . "<br>"; ?>
-Address: <?php echo $address . "<br>"; ?>
-Phone: <?php echo $phone . "<br>"; ?>
-
-This Notice is Given by Landlord:
-Name: <?php echo $issuer_name . "<br>"; ?>
-Address: <?php echo $issuer_address . "<br>"; ?>
-Phone: <?php echo $issuer_phone . "<br>"; ?>
-
-You are hereby given notice that you are behind on your rent or other amounts due. You are required to either pay everything owed or vacate the premises.
-
-Within three (3) business days (or the appropriate cure period as stated in the lease) you are required to:
-
-1. Pay the following:
-   a. Rent due: $<?php echo $amount_due . "<br>"; ?>
-   b. Late fees: $<?php echo $late_fees . "<br>"; ?>
-   c. Miscellaneous fees: $<?php echo $misc_fees . "<br>"; ?>
-   Total: $<?php echo $total . "<br>"; ?>
-
-OR
-
-2. All occupants must vacate the premises.
-
-If you do not comply with this notice, you will be served with a Summons and Complaint for unlawful detainer. Unlawful detainer is when you remain in possession of rental property after the owner serves you with a lawful notice to leave, such as this eviction notice. If you are found by the court to be in unlawful detainer, you will be evicted and found liable for all amounts owed under the lease and/or Utah law, plus attorney fees, court costs, and three times those damages allowed to be trebled under Utah Code Ann. §78B-6-811.
-
-If your lease requires mediation, you must alert us in writing within three days of your willingness to participate in mediation. Mediation shall take place within seven days of receipt of your written notification. If you fail to provide this written notification within three days and/or you fail to participate in mediation within seven days, be advised that your landlord intends to proceed with legal or equitable relief.
 
         </div>
         <div class="buttons">
